bd9c2d5d604f91b145dea31b2fd0bc69606d8bf5
Jesus Millan <jgmc3012@gmail.com>
2019-12-14 18:38:14 -0400

    :star: feat: Bases del Proyecto

---
+++ packages/demo/commands.py
@@ -0,0 +1,19 @@
+from cleo import Command
+from packages.core.utils.app_loop import AppLoop
+
+
+class AllCommands:
+    pass
+
+    # class Test(Command):
+    #     """
+    #     Test
+
+    #     demo:demo
+    #     """
+
+    #     def handle(self):
+    #         AppLoop().get_loop().run_until_complete(())
+
+    #     async def handleAsync(self):
+    #         await update_all_images_ml()
